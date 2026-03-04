74e5dfa649adff52dab7fa82a5d6da659bfbd753
Imira SR <shevon.eleesha@gmail.com>
2026-03-04 18:28:45 +0530

    fixing cors

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,18 +9,20 @@ router.post('/api/signup', authController.signup_post)
 
 router.get('/api/google', passport.authenticate('google', { scope: ['profile'] }))
 
-router.get('/api/google/callback', (req, res, next) => {
-    passport.authenticate('google', (err, user, info) => {
-        if (err) return next(err);
-        if (!user) return res.redirect('/'); // failure
+// router.get('/api/google/callback', (req, res, next) => {
+//     passport.authenticate('google', (err, user, info) => {
+//         if (err) return next(err);
+//         if (!user) return res.redirect('/'); // failure
 
-        // Log user in
-        req.logIn(user, (err) => {
-            if (err) return next(err);
-            return res.redirect(process.env.FRONTEND_URL); // success
-        });
-    })(req, res, next);
-});
+//         // Log user in
+//         req.logIn(user, (err) => {
+//             if (err) return next(err);
+//             return res.redirect(process.env.FRONTEND_URL); // success
+//         });
+//     })(req, res, next);
+// });
+
+router.get('/api/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => { res.redirect(`${process.env.FRONTEND_URL}`) })
 
 router.get('/api/logout', (req, res, next) => {
     req.logout(function (err) {
