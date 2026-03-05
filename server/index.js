13015c850b57ce53e09be3fe726263c088eb3753
Imira SR <shevon.eleesha@gmail.com>
2026-03-05 16:26:25 +0530

    fixing cron URL

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
-app.get('/api', (req, res) => res.send('hello'))
+app.get('/api/', (req, res) => res.send('hello'))
 app.get('/api/check_user', (req, res,) => {
   if (req.isAuthenticated()) {
     res.status(201).send('goood')
