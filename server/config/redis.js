5e7636283d876aa5d2c94c2aca8036a789476d73
Imira SR <shevon.eleesha@gmail.com>
2026-03-10 11:29:41 +0530

    changed env variables

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,7 +1,8 @@
 const { createClient } = require('redis')
+require('dotenv').config()
 
 const redisClient = createClient({
-  url: 'redis://default:PU7SHcBQHiIpdkFOBfpMq885c1JdyQ7C@redis-18738.c257.us-east-1-3.ec2.cloud.redislabs.com:18738'
+  url: process.env.REDIS_URL
 })
 
 module.exports = redisClient
